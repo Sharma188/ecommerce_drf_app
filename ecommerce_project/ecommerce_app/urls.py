6e7ae448243eb5e0bd9d5fd0d099a5b306bcd638
Harikrishan Sharma <harikrishansharma94@gmail.com>
2024-01-25 14:49:53 +0530

    updating url endpoints

--- ecommerce_project/ecommerce_app/urls.py
+++ ecommerce_project/ecommerce_app/urls.py
@@ -5,12 +5,12 @@ from django.conf import settings
 
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register('employees', views.UserViewSet)
-router.register('employees', views.ProductViewSet)
-router.register('employees', views.CategoryViewSet)
-router.register('employees', views.CartViewSet)
-router.register('employees', views.OrderViewSet)
-router.register('employees', views.FileUploadView)
+router.register('user', views.UserViewSet)
+router.register('product', views.ProductViewSet)
+router.register('category', views.CategoryViewSet)
+router.register('cart', views.CartViewSet)
+router.register('orders', views.OrderViewSet)
+router.register('files', views.FileUploadView)
 
 urlpatterns = [
     path('', include(router.urls))
